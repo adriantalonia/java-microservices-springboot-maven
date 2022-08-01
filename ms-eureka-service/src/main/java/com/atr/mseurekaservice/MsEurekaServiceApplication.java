5b9f1eb76a57a8290823ab817dfca8e17f6c741e
Adrian <adrian.talonia11@gmail.com>
2022-07-31 20:27:23 -0500

    add eureka, gateway and confir service

---
+++ ms-eureka-service/src/main/java/com/atr/mseurekaservice/MsEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atr.mseurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsEurekaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsEurekaServiceApplication.class, args);
+    }
+}
---
+++ ms-gateway-service/src/main/java/com/atr/msgatewayservice/MsGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.atr.msgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MsGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGatewayServiceApplication.class, args);
+    }
+
+}
